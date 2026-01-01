6fa4b57bfe155efc61c5da9e63278878eaec6678
wold9168 <moewold@outlook.com>
2026-01-02 05:25:32 +0800

    feat(caddy-config-manager): 照猫画虎修改代码业务逻辑
    修改 K8s 提供的演示逻辑。现在代码会列出集群中的所有ConfigMaps，并尝试从默认命名空间获取一个特定的示例ConfigMap。 此更改
    使代码与其管理Caddy配置的初衷保持一致。
    
    Signed-off-by: wold9168 <moewold@outlook.com>

--- sidecar/caddy-config-manager/main.go
+++ sidecar/caddy-config-manager/main.go
@@ -23,30 +23,33 @@ func main() {
 	}
 
 	for {
-		// 调用 CoreV1 API 的 Pods 接口，在所有命名空间（传入空字符串 "" 表示全部）中列出 Pod
-		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+		// 调用 CoreV1 API 的 ConfigMaps 接口，在所有命名空间（传入空字符串 "" 表示全部）中列出 configMaps
+		configMaps, err := clientset.CoreV1().ConfigMaps("").List(context.TODO(), metav1.ListOptions{})
 		if err != nil {
 			// 若 List 请求失败（如权限不足、API 不可用等），则 panic
 			panic(err.Error())
+			// TODO: 添加事前的鉴权检查，应该直接检查当前鉴权上下文是否支持读写 ConfigMaps
 		}
-		// 打印当前集群中 Pod 的总数
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+		// 打印当前集群中 configMaps 的总数
+		fmt.Printf("There are %d configMaps in the cluster\n", len(configMaps.Items))
 
-		// 演示如何处理特定 Pod 获取操作中的错误：
-		// 尝试从 "default" 命名空间中获取名为 "example-xxxxx" 的 Pod
-		_, err = clientset.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", metav1.GetOptions{})
-		// 如果错误是“未找到”（HTTP 404），则说明该 Pod 不存在
+		// 演示如何处理特定 configMap 获取操作中的错误：
+		// 尝试从 "default" 命名空间中获取名为 "example-xxxxx" 的 configMap
+		var namespace = "default"
+		var configMapName = "example"
+		_, err = clientset.CoreV1().ConfigMaps(namespace).Get(context.TODO(), configMapName, metav1.GetOptions{})
+		// 如果错误是“未找到”（HTTP 404），则说明该 configMap 不存在
 		if errors.IsNotFound(err) {
-			fmt.Printf("Pod example-xxxxx not found in default namespace\n")
+			fmt.Printf("ConfigMap example-xxxxx not found in default namespace\n")
 		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
 			// 如果错误是 Kubernetes API 返回的状态错误（如 403、500 等），则提取详细错误信息
-			fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
+			fmt.Printf("Error getting configMap %v\n", statusError.ErrStatus.Message)
 		} else if err != nil {
 			// 其他非 nil 错误（如网络问题、上下文取消等），直接 panic
 			panic(err.Error())
 		} else {
-			// 如果无错误，说明成功获取到 Pod
-			fmt.Printf("Found example-xxxxx pod in default namespace\n")
+			// 如果无错误，说明成功获取到 configMap
+			fmt.Printf("Found example-xxxxx configMap in default namespace\n")
 		}
 
 		// 每次循环后暂停 10 秒，避免对 API Server 造成过大压力
