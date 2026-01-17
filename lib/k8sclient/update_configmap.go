52baf58d87a9645856d0baefe0b824e0a64ba945
wold9168 <moewold@outlook.com>
2026-01-18 06:03:13 +0800

    chore(k8sclient, caddy-config-manager): 使得对 ConfigMap 的访问使用统一的 CRUD 接口
    Signed-off-by: wold9168 <moewold@outlook.com>

--- lib/k8sclient/update_configmap.go
+++ lib/k8sclient/update_configmap.go
@@ -1,13 +1,10 @@
 package k8sclient
 
 import (
-	"context"
-
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/klog/v2"
 )
 
@@ -17,13 +14,13 @@ const (
 )
 
 // updateExistingConfigMap updates an existing ConfigMap with new data
-func updateExistingConfigMap(ctx context.Context, configMaps corev1.ConfigMapInterface, existingCM *v1.ConfigMap, configMapName string, key string, data string) error {
+func updateExistingConfigMap(clientset kubernetes.Interface, namespace *string, existingCM *v1.ConfigMap, configMapName string, key string, data string) error {
 	if existingCM.Data == nil {
 		existingCM.Data = make(map[string]string)
 	}
 	existingCM.Data[key] = data
 
-	_, err := configMaps.Update(ctx, existingCM, metav1.UpdateOptions{})
+	_, err := UpdateExistingConfigMap(clientset, namespace, existingCM)
 	if err != nil {
 		klog.Errorf("Failed to update ConfigMap %s: %v", configMapName, err)
 		return err
@@ -33,17 +30,17 @@ func updateExistingConfigMap(ctx context.Context, configMaps corev1.ConfigMapInt
 }
 
 // createConfigMap creates a new ConfigMap with the specified data
-func createConfigMap(ctx context.Context, configMaps corev1.ConfigMapInterface, ns string, configMapName string, key string, data string) error {
+func createConfigMap(clientset kubernetes.Interface, ns *string, configMapName string, key string, data string) error {
 	newCM := &v1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      configMapName,
-			Namespace: ns,
+			Namespace: *ns,
 		},
 		Data: map[string]string{
 			key: data,
 		},
 	}
-	_, err := configMaps.Create(ctx, newCM, metav1.CreateOptions{})
+	_, err := CreateConfigMap(clientset, ns, newCM)
 	if err != nil {
 		klog.Errorf("Failed to create ConfigMap %s: %v", configMapName, err)
 		return err
@@ -54,16 +51,13 @@ func createConfigMap(ctx context.Context, configMaps corev1.ConfigMapInterface, 
 
 // UpdateConfigMap creates or updates a ConfigMap with the specified data
 func UpdateConfigMap(clientset kubernetes.Interface, namespaceProvided *string, configMapName string, key string, data string) error {
-	ctx := context.Background()
 	ns := GetCurrentNamespaceOrProvided(namespaceProvided)
 
-	configMaps := clientset.CoreV1().ConfigMaps(ns)
-
-	existingCM, err := configMaps.Get(ctx, configMapName, metav1.GetOptions{})
+	existingCM, err := GetConfigMap(clientset, &ns, configMapName)
 	if err == nil {
-		return updateExistingConfigMap(ctx, configMaps, existingCM, configMapName, key, data)
+		return updateExistingConfigMap(clientset, &ns, existingCM, configMapName, key, data)
 	} else if errors.IsNotFound(err) {
-		return createConfigMap(ctx, configMaps, ns, configMapName, key, data)
+		return createConfigMap(clientset, &ns, configMapName, key, data)
 	} else {
 		klog.Errorf("Failed to get ConfigMap %s: %v", configMapName, err)
 		return err
