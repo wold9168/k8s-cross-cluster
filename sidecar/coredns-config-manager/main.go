51a96620306505c97587c68b1353c3b15867286f
wold9168 <moewold@outlook.com>
2026-02-06 08:24:38 +0800

    fix(coredns-config-manager): 为 UpdateDNSRecordsForGateways 补充了错误处理
    Signed-off-by: wold9168 <moewold@outlook.com>

--- sidecar/coredns-config-manager/main.go
+++ sidecar/coredns-config-manager/main.go
@@ -76,7 +76,11 @@ func main() {
 
 		// 获取当前节点的 Tailscale 对端节点，并根据 HostName 生成 *.*.svc.HostName.remote 这样的 DNS 记录，装入我们上面拉起来的 DNS 服务器里
 		UpdateDNSRecordsForGateways(dnsSrv)
-		klog.Info("Records of internal DNS server have been updated.")
+		if err != nil {
+			klog.Info("Records of internal DNS server have been updated.")
+		} else {
+			klog.Infof("Updating records of internal DNS server failed. %w", err)
+		}
 
 		// 每次循环后暂停 10 秒，避免对 API Server 造成过大压力
 		time.Sleep(10 * time.Second)
